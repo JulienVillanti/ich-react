a07f38f7b1449f82710f907e96313a81dcdc3a7e
Jéssica Máximo <jessicamaximo23@gmail.com>
2024-05-14 14:52:01 -0400

    HomePage+AboutUsPage

--- src/Components/AboutUs/AboutUs.js
+++ src/Components/AboutUs/AboutUs.js
@@ -1,9 +1,25 @@
 import React from "react";
 import './AboutUs.css';
+import DaniellaOrnelasImg from "../Images/ProfileDani.jpeg";
 
 function AboutUs(){
     return(
-        <div>
+
+        <div className="aboutus-cointainer">
+            <h2>Sobre Nós</h2>
+
+            <div className="daniella-section">
+                <img src={DaniellaOrnelasImg} alt="Daniella Ornelas" className="daniella-img" />
+                <div className="daniella-info">
+                    <h3>Fisioterapeuta Daniella Ornelas</h3>
+                    <p>ajshbdjabsdjkabnsdkjasdjnasdjn.</p>
+                </div>
+            </div>
+
+            <div className="clinic-history">
+                <h3>História da Clínica</h3>
+                <p>Texto sobre a história da clínica e sua fundação.</p>
+            </div>
 
         </div>
 
