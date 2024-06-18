2119a080be53db9b5925e5fc1dd50d8592d53ac9
Jéssica Máximo <jessicamaximo23@gmail.com>
2024-06-18 07:54:04 -0400

    Calendar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import NavigationBar from "./Components/NavigationBar/NavigationBar";
 import HomePage from "./Components/HomePage/HomePage";
@@ -7,9 +7,11 @@ import ContactUs from "./Components/ContactUs/ContactUs";
 import Login from "./Components/Login/Login";
 import SignUp from "./Components/Sign-up/SignUp";
 import Services from "./Components/Services/Services";
+import Calendar from './Components/Calendar/Calendar.js';
 
 
 function App() {
+  
   return (
     <Router>
       <NavigationBar />
@@ -21,12 +23,22 @@ function App() {
         <Route path="/services" element={<Services />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/calendar" element={<Calendar />} />
 
 
       </Routes>
     </Router>
   );
 }
+
+
+// function Calendar() {
+
+//   return (
+
+//     <App />
+//   );
+// }
 
 export default App;
 
